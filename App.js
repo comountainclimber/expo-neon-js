7c5c641744f897f62eea984a17969ad255d49d85
Maxwell Lasky <mlasky46@gmail.com>
2020-08-06 08:06:22 -0600

    Adds to example

--- App.js
+++ App.js
@@ -2,12 +2,21 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import "./window-crypto";
-import Neon from "@cityofzion/neon-js";
+import Neon, { wallet, logging } from "@cityofzion/neon-js";
+
+const walletLogger = logging.logger.getLogger("wallet");
+logging.setAll("info");
+walletLogger.setLevel("info");
+
+const privateKey = Neon.create.privateKey();
+const account = new wallet.Account(privateKey);
+const w1 = Neon.create.wallet({ name: "myWallet" });
+w1.addAccount(account);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>{Neon.create.privateKey()}</Text>
+      <Text>{JSON.stringify(w1, null, 2)}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +25,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 12,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
